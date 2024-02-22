a7a7585fb2d55b20dbf2a85ea81edd1a13cc5124
pavanmmgs <pavanmuddala@gmail.com>
2024-02-22 17:56:44 +0530

    comments removed

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,6 @@ export default function Home() {
   return (
     <div>
       <ToDoList></ToDoList>
-      <h1>Sun Direct Portal</h1>
-      {/* <iframe width={"1400px"} height={"900px"} src='https://dashboard-test.videograph.ai/player_analytics?token=ewogICJvcmdfdXVpZCI6ICIxYzRkODE1ZS00OTg4LTRkNTktOTYxNC04YWE0YjZiOTQyZDEiLAogICJhdXRoX3Rva2VuIjogIkJhc2ljIFpESXhNR1V3TnpBdE5qSmhaaTAwTnpZNUxXRTJNVFl0WWpCak1qZGhNVFEzTkRjMk9taG5kRzAyYUdGb2JHTmhkWFkyY1hadGJBPT0iCn0='></iframe> */}
-      {/* <Yupptv></Yupptv> */}
     </div>
     
   )
